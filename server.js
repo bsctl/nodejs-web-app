60eb12f44ab05e0578f6fb7357383b0e5a5bdd0f
Adriano Pezzuto <kalise@users.noreply.github.com>
2017-02-06 09:47:32 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+//Simple web server in nodejs
+var http = require('http');
+var answer = process.env.MESSAGE;
+var server = http.createServer();
+const PORT = 8080
+
+//Define behaviour of the server receiving requests
+server.on('request', function(request,response){
+  console.log("Got request from: %s", request.connection.remoteAddress);
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'text/plain');
+  response.end(answer);
+});
+
+//Start the server
+server.listen(PORT, function(){
+    //Callback triggered when server is successfully listening
+    console.log("Server listening on: localhost:%s", PORT);
+});
