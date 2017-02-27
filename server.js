3b2af1934da3c17859dbdcc880219adabb14c9ed
Adriano Pezzuto <kalise@users.noreply.github.com>
2017-02-27 08:55:21 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 //Simple web server in nodejs
 var http = require('http');
+var ip = require('ip');
 var answer = process.env.MESSAGE;
 var server = http.createServer();
-const PORT = 8080
+const PORT = 8080;
+const HOST = ip.address();
 
 //Define behaviour of the server receiving requests
-server.on('request', function(request,response){
-  console.log("Got request from: %s", request.connection.remoteAddress);
-  response.statusCode = 200;
-  response.setHeader('Content-Type', 'text/plain');
-  response.end(answer + '\n');
+server.on('request', function(req,resp){
+  console.log("Got request from: %s", req.connection.remoteAddress);
+  resp.statusCode = 200;
+  resp.setHeader('Content-Type', 'text/plain');
+  resp.end(answer + " from " + HOST + "\n" );
 });
 
 //Start the server
 server.listen(PORT, function(){
     //Callback triggered when server is successfully listening
-    console.log("Server listening on: localhost:%s", PORT);
+    console.log("Server listening on: %s:%s", HOST,PORT);
 });
